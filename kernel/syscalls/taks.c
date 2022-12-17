5c9685ceb30c390fd1aea114af4f33726ce3ac13
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-17 18:03:01 +0100

    reorganized syscalls module

--- kernel/syscalls/taks.c
+++ kernel/syscalls/taks.c
@@ -7,8 +7,8 @@ This enum lists all the services that can be requested by an application to
 the kernel.
 Each service has a numeric identifier.
 */
-typedef enum
-{
+
+typedef enum {
     CREATE_TASK_ID = 1,
 } SysCallID;
 
@@ -40,37 +40,37 @@ void unknownService(void) {
 /*
 
 kcreate_task(), brief description:
-    This is the function used by the kernel to create a new task
-    The functions pushes onto the task's empty stack the initial values
-    for its register. Then the task is added to the tasks queue.
+This is the function used by the kernel to create a new task
+The functions pushes onto the task's empty stack the initial values
+for its register. Then the task is added to the tasks queue.
 
-    Registers layout for the cortex-M4 processor:
-        r0 function argument 1 / general purpose
-        r1 function argument 2 / general purpose
-        r2 function argument 3 / general purpose
-        r3 function argument 4 / general purpose
-        r4 --
-        r5 |
-        r6 |
-        r7 |
-        r8 | General purpose
-        r9 |
-        r10 |
-        r11 |
-        r12 --
-        r13 stack pointer
-        r14 link register
-        r15 program counter
+Registers layout for the cortex-M4 processor:
+    r0 function argument 1 / general purpose
+    r1 function argument 2 / general purpose
+    r2 function argument 3 / general purpose
+    r3 function argument 4 / general purpose
+    r4 --
+    r5 |
+    r6 |
+    r7 |
+    r8 | General purpose
+    r9 |
+    r10 |
+    r11 |
+    r12 --
+    r13 stack pointer
+    r14 link register
+    r15 program counter
 
-    Task initialization:
-    When a task is prehempted by the SysTick interrupt handler, its
-    program counter is saved in the link register. Then the SysTick
-    handler calls the routine that performs the context switch, which
-    pushes r0 through r12, and r14 (link register) onto the stack.
-    Therefore a new task's stack needs to be initialized by pushing
-    the necessary values for registers r0-r12 and for the link register,
-    which should hold the memory address of the first instruction to be
-    executed by the task.
+Task initialization:
+When a task is prehempted by the SysTick interrupt handler, its
+program counter is saved in the link register. Then the SysTick
+handler calls the routine that performs the context switch, which
+pushes r0 through r12, and r14 (link register) onto the stack.
+Therefore a new task's stack needs to be initialized by pushing
+the necessary values for registers r0-r12 and for the link register,
+which should hold the memory address of the first instruction to be
+executed by the task.
 */
 
 void kcreate_task(void (*code)(void *), void *args, size_t priority) {
