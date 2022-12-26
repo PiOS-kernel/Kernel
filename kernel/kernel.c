2470e1466e128e2589877948e9ce7872fa2f6340
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-26 16:59:28 +0100

    improved kernel initialization

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,10 +2,19 @@
 
 uint8_t HEAP_MEMORY[HEAP_SIZE];
 Heap HEAP;
-struct TaskTCB* RUNNING = NULL;
+struct TaskTCB* RUNNING;
 Queue WAITING_QUEUES[MIN_PRIORITY];
 Queue READY_QUEUES[MIN_PRIORITY];
 
 void kernel_init() {
+    // Initialize the heap
+    HEAP.head = NULL;
     heap_init(&HEAP, HEAP_MEMORY, HEAP_SIZE);
+
+    // Initialize scheduler data structures
+    RUNNING = NULL;
+    for (int i=0; i<MIN_PRIORITY; i++) {
+        Queue_init(&WAITING_QUEUES[i]);
+        Queue_init(&READY_QUEUES[i]);
+    }
 }
