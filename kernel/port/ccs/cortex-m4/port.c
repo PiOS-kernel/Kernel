637d980e71eafb7c088b18196f8dc38390d3fc19
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-29 15:13:05 +0100

    added port specific implementations of kcreate task

---
+++ kernel/port/ccs/cortex-m4/port.c
@@ -0,0 +1,59 @@
+#include "../../../task/task.h"
+
+/* 
+On exit from an exception or interrupt handler, when the cortex m4 processor
+is running in thumb mode it expects bit[0] of the next instruction to execute to
+be set to 0.
+*/
+
+#define PC_MASK 0xFFFFFFFE
+
+/* This is the initial value for the XPSR register of a newly created task. */
+const uint32_t INITIAL_XPSR = 0x01000000;
+
+/* 
+When a task is created its stack is initialized to resemble what it would
+look like if the task was interrupted by an interrupt handler. This is
+the value for exception return.
+WE WILL NEED TO CHANGE THIS VALUE ONCE WE USE 'MSP' and 'PSP'.
+*/
+#define INITIAL_EXEC_RETURN 0xFFFFFFFD
+
+/* Port specific implementation of kcreate_task */
+
+void kcreate_task(void (*code)(void *), void *args, uint8_t priority) {
+    // The task's TCB is created
+    TaskTCB* tcb = (TaskTCB*) alloc(sizeof(TaskTCB));
+    TaskTCB_init(tcb, priority);
+
+    uint32_t zeros[12];
+    memset((uint8_t*) zeros, 0, sizeof(uint32_t) * 12);
+
+    // The XPSR register is pushed onto the stack.
+    tcb->stp -= 4;
+    *((uint32_t*) tcb->stp) = INITIAL_XPSR;
+
+    // The program counter is pushed onto the stack. It will contain a
+    // pointer to the task's entry point.
+    tcb->stp -= 4;
+    *((uint32_t*) tcb->stp) = (uint32_t) code & PC_MASK;
+
+    // Then the link register is pushed and initialized to 0.
+    stack_push(tcb, (uint8_t*) zeros, sizeof(uint32_t));
+
+    // Then r12, r3, r2, r1, and r0 are pushed onto the stack. Only r0
+    // has a non-zero value, which is the pointer to the arguments.
+    stack_push(tcb, (uint8_t*) zeros, sizeof(uint32_t) * 4);
+    stack_push(tcb, (uint8_t*) &args, sizeof(void *));
+
+    // Now the execution return value from the ISR is pushed onto the stack.
+    tcb->stp -= 4;
+    *((uint32_t*) tcb->stp) = INITIAL_EXEC_RETURN;
+
+    // Finally, registers r4 through r11 are pushed onto the stack and
+    // 0-initialized.
+    stack_push(tcb, (uint8_t*) zeros, sizeof(size_t) * 8);
+
+    // The task is inserted into the tasks queue
+    enqueue(&READY_QUEUES[priority], tcb);
+}
--- kernel/syscalls/task_syscalls.c
+++ kernel/syscalls/task_syscalls.c
@@ -6,24 +6,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* 
-When a task is created its stack is initialized to resemble what it would
-look like if the task was interrupted by an interrupt handler. This is
-the value for exception return.
-WE WILL NEED TO CHANGE THIS VALUE ONCE WE USE 'MSP' and 'PSP'.
-*/
-#define INITIAL_EXEC_RETURN 0xFFFFFFF9
-
-/* 
-On exit from an exception or interrupt handler, when the cortex m4 processor
-is running in thumb mode it expects bit[0] of the next instruction to execute to
-be set to 0.
-*/
-#define PC_MASK 0xFFFFFFFE
-
-/* This is the initial value for the XPSR register of a newly created task. */
-const uint32_t INITIAL_XPSR = 0x01000000;
-
 /*
 This is the system call provided to the user application, in order to
 create a new task.
@@ -65,48 +47,12 @@ Registers layout for the cortex-M4 processor:
     r14 link register
     r15 program counter
 
-Task initialization:
 The initialization of a new task's stack is done by mimicking the
 image of the stack when program execution is interruped by an 
 interrupt handler.
 */
 
-void kcreate_task(void (*code)(void *), void *args, uint8_t priority) {
-    // The task's TCB is created
-    TaskTCB* tcb = (TaskTCB*) alloc(sizeof(TaskTCB));
-    TaskTCB_init(tcb, priority);
-
-    uint32_t zeros[12];
-    memset((uint8_t*) zeros, 0, sizeof(uint32_t) * 12);
-
-    // The XPSR register is pushed onto the stack.
-    tcb->stp -= 4;
-    *((uint32_t*) tcb->stp) = INITIAL_XPSR;
-
-    // The program counter is pushed onto the stack. It will contain a
-    // pointer to the task's entry point.
-    tcb->stp -= 4;
-    *((uint32_t*) tcb->stp) = (uint32_t) code & PC_MASK;
-
-    // Then the link register is pushed and initialized to 0.
-    stack_push(tcb, (uint8_t*) zeros, sizeof(uint32_t));
-
-    // Then r12, r3, r2, r1, and r0 are pushed onto the stack. Only r0
-    // has a non-zero value, which is the pointer to the arguments.
-    stack_push(tcb, (uint8_t*) zeros, sizeof(uint32_t) * 4);
-    stack_push(tcb, (uint8_t*) &args, sizeof(void *));
-
-    // Now the execution return value from the ISR is pushed onto the stack.
-    tcb->stp -= 4;
-    *((uint32_t*) tcb->stp) = INITIAL_EXEC_RETURN;
-
-    // Finally, registers r4 through r11 are pushed onto the stack and
-    // 0-initialized.
-    stack_push(tcb, (uint8_t*) zeros, sizeof(size_t) * 8);
-
-    // The task is inserted into the tasks queue
-    enqueue(&READY_QUEUES[priority], tcb);
-}
+extern void kcreate_task(void (*code)(void *), void *args, uint8_t priority);
 
 /*
 This is the kernel space implementation of the exit() system call.
