089b04858da07f98324c1513064f8b69d08d65eb
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2023-01-06 11:43:32 +0100

    messages can be delivered with events

--- kernel/synch/events.c
+++ kernel/synch/events.c
@@ -1,17 +1,27 @@
 #include "events.h"
 #include "../kernel.h"
-
+#include "../utils/utils.h"
 
 /* 
 @brief: creates an event object and returns an handle.
+
+@param: msg_size - the size of the message.
 */
 
-EventHandle new_event() {
-    Event* event = alloc(sizeof(Event));
+EventHandle new_event(uint32_t msg_size) {
+    Event* event = alloc(sizeof(Event) + msg_size);
 
     // There is no handling of allocation failure because if no memory
     // is available, the system cannot be recovered to a valid state.
 
+    // The message that comes with the event is stored just after the
+    // event object itself. This is done in order to minimize heap
+    // fragmentation.
+    // This implementation detail is hidden from the application. The
+    // application will only access the messagge through the dedicated
+    // API.
+    event->msg_ptr = (void*) ((uint32_t) event + sizeof(Event));
+    event->msg_size = msg_size;
     Queue_init(&event->waiting_queue);
 
     return (EventHandle) event;
@@ -52,12 +62,14 @@ void event_wait(EventHandle event) {
 @param: event - the event handle
 */
 
-void event_post(EventHandle event) {
+void event_post(EventHandle event, void* msg_ptr) {
     Event* e = (Event*) event;
 
     // INTERRUPTS MUST BE DISABLED. Transactions to the shared running
     // queue must be atomic.
     disable_interrupts();
+
+    memcpy((uint8_t*) msg_ptr, (uint8_t*) e->msg_ptr, e->msg_size);
 
     // All tasks in the waiting queue are woken up
     while (!empty(&e->waiting_queue)) {
@@ -67,4 +79,16 @@ void event_post(EventHandle event) {
 
     // Interrupts are enabled again
     enable_interrupts();
+}
+
+/*
+@brief: the event message is copied to the destination buffer.
+
+@param: event - the event handle
+@param: dst - the destination buffer
+*/
+
+void get_event_msg(EventHandle event, void* dst) {
+    Event* e = (Event*) event;
+    memcpy((uint8_t*) dst, (uint8_t*) e->msg_ptr, e->msg_size);
 }
