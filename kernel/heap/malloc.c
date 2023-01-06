02abaddbb086f06b92042dafa7830b5ec3a7f0e2
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2023-01-06 18:09:30 +0100

    BREAKING CHANGE to free: it is now a function and it also takes as argument the amount of memory to deallocate

--- kernel/heap/malloc.c
+++ kernel/heap/malloc.c
@@ -1,7 +1,29 @@
 #include "malloc.h"
 
-/* The great `malloc` */
+/**
+ * @brief: Memory allocation primitive for a PiOS application
+ * @param: the amount of memory to be allocated
+ * @return: a pointer to the allocated memory
+*/
 
 uint8_t* alloc(size_t size) {
     return allocate_segment(&HEAP, size);
+}
+
+/**
+ * @brief: De-allocation primitive 
+ * 
+ * @param: the memory to be de-allocated 
+ * @param: the amount of memory to be de-allocated
+*/
+
+void free(uint8_t* ptr, size_t size) {
+    // The minimum amount of memory allocated is equal to SEGMENT_HEADER_SIZE
+    if (size < SEGMENT_HEADER_SIZE)
+        size = SEGMENT_HEADER_SIZE;
+    
+    // Memory is allocated to 4-byte boundaries
+    size = (size + 3) & ~3;
+
+    free_segment(&HEAP, ptr, size);
 }
--- kernel/heap/malloc.h
+++ kernel/heap/malloc.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include "heap.h"
 
-#define free(ptr) if (ptr != NULL) {free_segment(&HEAP, (uint8_t*) ptr, sizeof(*ptr) > SEGMENT_HEADER_SIZE ? sizeof(*ptr) : SEGMENT_HEADER_SIZE);}
 uint8_t* alloc(size_t size);
+void free(uint8_t* ptr, size_t size);
 
 #endif
