28fcfba35c1242fd52f53337e17e99105e76b127
Francesco Maglie <francesco.maglie@studenti.unitn.it>
2023-01-13 19:45:36 +0100

    fix synch: add update priority in mutex post

--- kernel/synch/synch.c
+++ kernel/synch/synch.c
@@ -58,6 +58,7 @@ uint8_t mutex_post(MCB* lock){
     if(lock->lock == 1 && dynamicList_search(lock->owners, (uint32_t) RUNNING)!=NULL){ // mutex has just 1 element
         lock->lock = 0;
         dynamicList_remove(lock->owners, (uint32_t) RUNNING);
+        update_max_ownerPriority(lock);
         enable_interrupts();
         return 1;
     }
