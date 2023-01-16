cf27d6e37d423a1940d2aaaafd8e5dfbbe974e75
marco <marco.barbieri@studenti.unitn.it>
2023-01-16 11:48:58 +0100

    user defined msg_type and pipe size

--- kernel/synch/pipe.h
+++ kernel/synch/pipe.h
@@ -3,12 +3,11 @@
 
 #include "synch.h"
 
-#define MESSAGE_SIZE 64
-#define PIPE_SIZE 4
+#define NEW_PIPE(lenght, msg_type) init_pipe(lenght, sizeof(msg_type))
 
 //struct to create a MESSAGE type
 typedef struct MESSAGE {
-  char data[MESSAGE_SIZE];
+  char data[];
 } MESSAGE;
 
 //struct defining the pipe as a ciclic message queue 
@@ -16,15 +15,17 @@ typedef struct PIPE {
   int start;
   int end;
   int current_load;
-  MESSAGE messages[PIPE_SIZE];
   MCB *read_mutex;
   MCB *write_mutex;
   Queue waiting_on_read;
   TaskTCB *waiting_on_write;
+  uint32_t msg_size;
+  uint32_t pipe_size;
+  char* messages;
 } PIPE;
 
-void init_pipe(PIPE *pipe);
-void pub_msg(PIPE *pipe, MESSAGE *msg);
+PIPE *init_pipe(int lenght, uint32_t msg_size);
+void pub_msg(PIPE *pipe, MESSAGE *msg); 
 void read_msg(PIPE *pipe, MESSAGE *msg);
 void wait_reading(PIPE *pipe);
 void wait_writing(PIPE *pipe);
